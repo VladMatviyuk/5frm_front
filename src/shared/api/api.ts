8d5f09828da7626a0f35694b015c2e9145458dd2
vlad <vladm1225@yandex.ru>
2025-07-20 22:12:42 +0400

    fix back

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const getBaseURL = () => {
-	return import.meta.env.VITE_API_URL;
+	return import.meta.env.VITE_API_URL || 'http://62.113.41.218:3000';
 };
 
 const createClient = (withAuth = false): AxiosInstance => {
